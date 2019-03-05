664bd2fddd4b1301a8b51478c799860d062c9921
baochongying <36734455+baochongying@users.noreply.github.com>
2019-03-06 03:08:27 +0800

    Add files via upload

---
+++ python3/Students.py
@@ -0,0 +1,10 @@
+class Student():
+    name = "包崇应"
+    age = 39
+    #    def eat(self,name,age):
+    #        print (name,"can eat"+"ande he is ",age)
+    def eat(self):
+        print (self.name, "can eat", "ande he is ", self.age)
+
+
+Student().eat()
