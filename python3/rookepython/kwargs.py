a9667b6d11124f2f82bf1d527b2dfc1a9de1e3d2
root <chongyingbao@gmail.com>
2019-03-07 19:18:14 +0800

    python3 code 2019/3/7 update

--- python3/rookepython/kwargs.py
+++ python3/rookepython/kwargs.py
@@ -1,8 +1,14 @@
-"""kwargs"""
+"""this is **args"""
 def m1(*args,**kwargs):
-    print "the type of",type(args)
-    print ("the type of ",type(kwargs))
+   print "the type of",type(args)
+   print ("the type of ",type(kwargs))
 #m1()
-dic_inty={'name':'inty','age':'18 years old','height':'190cm'}
-for k,v in dic_inty.items():
-    print (k,':',v)
+#dic_inty={'name':'inty','age':'18 years old','height':'190cm','weght':'195lb'}
+
+#def someone(dic_someone):
+ #   for k,v in dic_someone.items():
+#for k,v in dic_inty.items():
+def someone(**kwargs):
+    for  k ,v in kwargs.items():
+        print (k, ':', v)
+someone(name='xiaohong',age='65',herght="65")
